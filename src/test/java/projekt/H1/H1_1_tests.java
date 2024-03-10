5018abeb3676d5a386ec12e5dd82a12714555dd0
Bach Alexander <dargonrol@gmail.com>
2024-03-10 02:46:20 +0100

    added JUnit Test for addResources

--- src/test/java/projekt/H1/H1_1_tests.java
+++ src/test/java/projekt/H1/H1_1_tests.java
@@ -8,12 +8,25 @@ import org.junit.jupiter.api.Assertions;
 
 public class H1_1_tests {
     @Test
-    void H1_1_test() {
-        // method addResource
+    void H1_1_addResource() {
         PlayerImpl.Builder playerBuilder = new PlayerImpl.Builder(0);
         Player player = playerBuilder.build(null);
         player.addResource(ResourceType.CLAY, 20);
         Assertions.assertEquals(player.getResources().get(ResourceType.CLAY), 20);
+        player.addResource(ResourceType.GRAIN, -2);
+        Assertions.assertEquals(player.getResources().get(ResourceType.GRAIN), 0);
+        player.addResource(ResourceType.ORE, 0);
+        Assertions.assertEquals(player.getResources().get(ResourceType.ORE), 0);
+    }
 
+    @Test
+    void H1_1_addResources() {
+        PlayerImpl.Builder playerBuilder = new PlayerImpl.Builder(0);
+        Player player = playerBuilder.build(null);
+        player.addResources(resources.randomResources1);
+        Assertions.assertEquals(player.getResources().get(ResourceType.CLAY), 10);
+        Assertions.assertEquals(player.getResources().get(ResourceType.ORE), 5);
+        Assertions.assertEquals(player.getResources().get(ResourceType.GRAIN), 0);
+        Assertions.assertEquals(player.getResources().get(ResourceType.WOOD), 0);
     }
 }
